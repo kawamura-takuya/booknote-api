ffc76f386f7430e66c0e3e243d25403ccd526a3a
calmdown1987 <calmdown1987@icloud.com>
2025-12-04 08:39:51 +0900

    ユーザー新規登録API用のUsersControllerとルーティングを追加

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::UsersController < ApplicationController
+  
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: {
+        message: "ユーザーの仮登録が完了しました"
+      }, status: :created
+    else
+      render json: {
+        errors: user.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
+    devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+    namespace :api do
+      namespace :v1 do
+        resources :users, only: [:create]
+      end
+    end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
